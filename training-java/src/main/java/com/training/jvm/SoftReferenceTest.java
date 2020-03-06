2827d2901b47536e883dfc7686b103c39e5c3c72
ganjx <415032090@qq.com>
2020-03-06 14:13:51 +0800

    增加软引用和弱引用用例

---
+++ training-java/src/main/java/com/training/jvm/SoftReferenceTest.java
@@ -0,0 +1,41 @@
+package com.training.jvm;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * @author ganjx
+ * Copyright (c) 2012-2020 All Rights Reserved.
+ */
+public class SoftReferenceTest {
+
+    /**
+     * 软引用是用来描述一些还有用但并非必须的对象。对于软引用关联着的对象，
+     * 在系统将要发生内存溢出异常之前，将会把这些对象列进回收范围进行第二次回收。
+     * 如果这次回收还没有足够的内存，才会抛出内存溢出异常。
+     * 运行参数 -Xmx200m -XX:+PrintGC
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        //100M的缓存数据
+        byte[] cacheData = new byte[100 * 1024 * 1024];
+        //将缓存数据用软引用持有
+        SoftReference<byte[]> cacheRef = new SoftReference<>(cacheData);
+        //将缓存数据的强引用去除
+        cacheData = null;
+        System.out.println("第一次GC前" + cacheData);
+        System.out.println("第一次GC前" + cacheRef.get());
+        //进行一次GC后查看对象的回收情况
+        System.gc();
+        //等待GC
+        Thread.sleep(500);
+        System.out.println("第一次GC后" + cacheData);
+        System.out.println("第一次GC后" + cacheRef.get());
+
+        //在分配一个120M的对象，看看缓存对象的回收情况
+        byte[] newData = new byte[120 * 1024 * 1024];
+        System.out.println("分配后" + cacheData);
+        System.out.println("分配后" + cacheRef.get());
+    }
+}
--- training-java/src/main/java/com/training/jvm/WeakReferenceTest.java
+++ training-java/src/main/java/com/training/jvm/WeakReferenceTest.java
@@ -8,27 +8,33 @@ import java.lang.ref.WeakReference;
  */
 public class WeakReferenceTest {
 
+    /**
+     * 弱引用也是用来描述非必须对象的，他的强度比软引用更弱一些，被弱引用关联的对象，
+     * 在垃圾回收时，如果这个对象只被弱引用关联（没有任何强引用关联他），那么这个对象就会被回收。
+     *
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
-        A a = new A();
-        B b = new B(a);
-        a = null;
-        System.out.println(b.getA());  // not null
+        //100M的缓存数据
+        byte[] cacheData = new byte[100 * 1024 * 1024];
+        //将缓存数据用软引用持有
+        WeakReference<byte[]> cacheRef = new WeakReference<>(cacheData);
+        System.out.println("第一次GC前" + cacheData);
+        System.out.println("第一次GC前" + cacheRef.get());
+        //进行一次GC后查看对象的回收情况
         System.gc();
-        System.out.println(b.getA());  // null
-    }
-}
+        //等待GC
+        Thread.sleep(500);
+        System.out.println("第一次GC后" + cacheData);
+        System.out.println("第一次GC后" + cacheRef.get());
 
-class A {}
-
-class B {
-
-    WeakReference<A> weakReference;
-
-    public B(A a) {
-        weakReference = new WeakReference<>(a);
-    }
-
-    public A getA() {
-        return weakReference.get();
+        //将缓存数据的强引用去除
+        cacheData = null;
+        System.gc();
+        //等待GC
+        Thread.sleep(500);
+        System.out.println("第二次GC后" + cacheData);
+        System.out.println("第二次GC后" + cacheRef.get());
     }
 }
