70916102921afdd9d7cbba352ae21ed8871988ca
ganjx <415032090@qq.com>
2020-03-10 18:03:10 +0800

    死锁示例

---
+++ training-java/src/main/java/com/training/thread/DeadLockTest.java
@@ -0,0 +1,62 @@
+package com.training.thread;
+
+/**
+ * @author ganjx
+ * Copyright (c) 2012-2020 All Rights Reserved.
+ */
+public class DeadLockTest {
+
+    public void run(){
+        MyThread thread1 = new MyThread();
+        new Thread(thread1,"Cat").start();
+        new Thread(thread1,"Tom").start();
+
+    }
+
+    class  MyThread implements  Runnable{
+
+        private  Object o1 = new Object();
+        private  Object o2 = new Object();
+        private  boolean flag = true;
+
+        @Override
+        public void run() {
+            if(flag){
+                flag = false;
+                synchronized (o1){
+                    System.out.println(Thread.currentThread().getName() + " have o1.");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (o2){
+                        System.out.println(Thread.currentThread().getName() + " have o2.");
+                    }
+
+                }
+            }
+            else{
+                synchronized (o2){
+                    System.out.println(Thread.currentThread().getName() + " have o2.");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    synchronized (o1){
+                        System.out.println(Thread.currentThread().getName() + " have o1.");
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new DeadLockTest().run();
+    }
+
+}
