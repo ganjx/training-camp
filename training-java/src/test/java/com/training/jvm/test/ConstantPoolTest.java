bee4512e8c95d3c0f8fa99943f0f11668b53b2f3
ganjx <415032090@qq.com>
2020-03-19 21:08:56 +0800

    rollback

---
+++ training-java/src/test/java/com/training/jvm/test/ConstantPoolTest.java
@@ -0,0 +1,13 @@
+package com.training.jvm.test;
+
+/**
+ * @author ganjx
+ * Copyright (c) 2012-2020 All Rights Reserved.
+ */
+public class ConstantPoolTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
