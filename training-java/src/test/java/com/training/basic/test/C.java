8a5a22ce9549251b2b4c807efe0ec42aac31d35f
ganjx <415032090@qq.com>
2020-03-24 21:53:10 +0800

    add test

---
+++ training-java/src/test/java/com/training/basic/test/C.java
@@ -0,0 +1,37 @@
+package com.training.basic.test;
+
+/**
+ * @author ganjx
+ * Copyright (c) 2012-2020 All Rights Reserved.
+ */
+public class C {
+    C(){
+        System.out.println("C");
+    }
+}
+
+class A {
+    C c = new C();
+
+    A() {
+        this("A");
+        System.out.print("A");
+    }
+
+    A(String s) {
+        System.out.print(s);
+    }
+
+    C c2 = new C();
+}
+
+class Test extends A {
+    Test() {
+        super("B");
+        System.out.print("B");
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+}
