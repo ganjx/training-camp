6668a52d27f7c3b89eb4ccd1ba765cc2c88f8fb4
ganjx <415032090@qq.com>
2020-04-03 11:30:08 +0800

    add springboot

---
+++ training-sprintboot/src/main/java/com/training/springboot/trainingsprintboot/TrainingSprintbootApplication.java
@@ -0,0 +1,13 @@
+package com.training.springboot.trainingsprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSprintbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSprintbootApplication.class, args);
+    }
+
+}
---
+++ training-sprintboot/src/test/java/com/training/springboot/trainingsprintboot/TrainingSprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.springboot.trainingsprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingSprintbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
