b25647bfb9875dfe384749bb891f90f334549153
ganjx <415032090@qq.com>
2020-04-14 21:20:45 +0800

    add DeleteMiddleNodeLcci

---
+++ training-java/src/main/java/com/training/leetcode/simple/DeleteMiddleNodeLcci.java
@@ -0,0 +1,52 @@
+package com.training.leetcode.simple;
+
+/**
+ * @author ganjx
+ * Copyright (c) 2012-2020 All Rights Reserved.
+ */
+public class DeleteMiddleNodeLcci {
+
+
+    /**
+     * 实现一种算法，删除单向链表中间的某个节点（除了第一个和最后一个节点，不一定是中间节点），假定你只能访问该节点。
+     * 示例：
+     * <p>
+     * 输入：单向链表a->b->c->d->e->f中的节点c
+     * 结果：不返回任何数据，但该链表变为a->b->d->e->f
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/delete-middle-node-lcci
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        deleteNode(listNode2);
+
+        print(listNode1);
+    }
+
+    public static void print(ListNode head) {
+        System.out.println(head.val);
+        if (head.next != null) {
+            print(head.next);
+        }
+    }
+
+    public static void deleteNode(ListNode node) {
+        if (null == node || null == node.next) return;
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+}
